bc118395af326c0a2deb7eea44f04350a7c77df2
Leon Fausten <leon.fausten@gmail.com>
2015-01-12 21:06:19 +0100

    Game State

---
+++ TTV/Aufgabe2_3/src/de/haw/connectfour/BoardState.java
@@ -0,0 +1,40 @@
+package de.haw.connectfour;
+
+public class BoardState {
+	
+	FieldState[] board;
+	
+	public BoardState(int size){
+		board = new FieldState[size];
+		for(int i = 0; i < size; i++)
+			board[i] = FieldState.WATER;
+	}
+	
+	public BoardState(FieldState[] init){
+		board = init;
+	}
+	
+	public FieldState[] getState(){
+		return board;
+	}
+	
+	public FieldState getPositionState(int position){
+		if(position <= board.length+1)
+			return board[position+1];
+		else
+			return FieldState.UNKNOWN;
+	}
+	
+	public void setShip(int position){
+		board[position-1] = FieldState.SHIP;
+	}
+	
+	public FieldState fire(int position){
+		if(board[position-1] == FieldState.SHIP){
+			board[position-1] = FieldState.HIT;
+			return FieldState.HIT;
+		}else {
+			return FieldState.WATER;
+		}		
+	}
+}
---
+++ TTV/Aufgabe2_3/src/de/haw/connectfour/FieldState.java
@@ -0,0 +1,8 @@
+package de.haw.connectfour;
+
+public enum FieldState {
+	HIT,
+	WATER,
+	SHIP,
+	UNKNOWN
+}
